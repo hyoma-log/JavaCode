3a5efb448daa1dc30e74542d3b61b4efaf0de107
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-03-19 23:48:04 +0900

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,21 @@
+// javaの四則演算
+// public class Hello {
+//   public static void main(String[] args) {
+//     System.out.println((2 + 3) * 3);
+//     System.out.println(2 - 3);
+//     System.out.println(2 * 3);
+//     System.out.println(2 / 3);
+//   }
+// }
+
+// 変数にデータを入れる
+// public class Hello {
+//   public static void main(String[] args) {
+//     String name = "world";
+//     int a = 10;
+//     int b = 20;
+//     System.out.println(a + b);
+//     System.out.println("Hello, " + name + "!");
+//   }
+// }
+
